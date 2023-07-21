ff22edbf96d5e186a0d5e2078cff072c2edd29b3
harisantoso <harysanttos@gmail.com>
2023-07-21 20:28:55 +0700

    review section

--- js/script.js
+++ js/script.js
@@ -44,8 +44,8 @@ window.onscroll = () =>{
   navbar.classList.remove('active');
 }
 
-// <!-- products section starts  -->
-let swiper = new Swiper(".product-slider", {
+// products section starts 
+var swiper = new Swiper(".product-slider, .review-slider",{
   loop: false,
   spaceBetween: 20,
   autoplay: {
@@ -65,4 +65,28 @@ let swiper = new Swiper(".product-slider", {
     },
   },
 });
-// <!-- products section ends  -->
+// products section ends 
+
+
+// review section start 
+// var swiper = new Swiper(".review-slider", {
+//   loop: false,
+//   spaceBetween: 20,
+//   autoplay: {
+//     delay: 7500,
+//     disableOnInteraction: false,
+//   },
+//   centerdSlides: true,
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 1,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1020: {
+//       slidesPerView: 3,
+//     },
+//   },
+// });
+// review section end
